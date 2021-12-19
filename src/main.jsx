031f2c640ea171dc15b25c6815e4b32ede48aaf6
Progra-maria <78096795+Progra-maria@users.noreply.github.com>
2021-12-19 19:56:57 +0100

    ../dist

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import '/dist/output.css'
+import '../dist/output.css'
 
 ReactDOM.render(
   <React.StrictMode>
